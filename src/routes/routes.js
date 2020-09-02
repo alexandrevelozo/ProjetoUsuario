4acf915db432be822bf6a58b79ab21527d7700b3
alexandrevelozo <alexandrevelozo4@gmail.com>
2020-09-01 21:27:30 -0300

    project-users

---
+++ src/routes/routes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const UsuarioController = require('../controllers/usuarioController.js');
+const router = express.Router();
+
+router.post('/usuarios', UsuarioController.Insert);
+
+module.exports = router;
+
+//HTTP
+
+// Request = Requisição
+// POST = Inserir
+// GET = Selecionar/Exibir
+// PUT = Atualizar
+// DELETE = Excluir
+
+// Response = Resposta
+// Status code
+
+//
+// 200 = ok
+// 300 = err
+// 400 = not found
+// 500 = server error
